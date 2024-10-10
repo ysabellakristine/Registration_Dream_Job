19eef48cb5a08ba0e6658e1ed758874afe646391
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-10-10 14:26:28 +0800

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE, 
+    role VARCHAR(50) NOT NULL,
+    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
